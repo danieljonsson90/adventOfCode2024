8067441797861b9aa20eba43c73089a6b9d9c6c5
Daniel J <daniel.jonsson@consid.se>
2025-03-24 11:31:33 +0100

    day9 sol2

--- src/functions/day9.ts
+++ src/functions/day9.ts
@@ -17,6 +17,63 @@ export const day9ProblemOne = (data: string, result: number): number => {
   return result;
 };
 
+export const day9ProblemTwo = (data: string, result: number): number => {
+  const list = processData(data);
+  const fileMap = new Map<string, { numbers: string[]; position: number }>();
+  let freeSpaces: { start: number; length: number }[] = [];
+  for (const [i, item] of list.entries()) {
+    if (item !== '.') {
+      if (fileMap.get(item)) {
+        fileMap.get(item)?.numbers.push(item);
+      } else {
+        fileMap.set(item, { numbers: [item], position: i });
+      }
+    }
+  }
+
+  let start = -1;
+  for (const [i, item] of list.entries()) {
+    if (item === '.') {
+      if (start === -1) {
+        start = i;
+      }
+    } else {
+      if (start !== -1) {
+        freeSpaces.push({ start: start, length: i - start });
+        start = -1;
+      }
+    }
+  }
+
+  if (start !== -1) freeSpaces.push({ start, length: list.length - start });
+
+  const sortedFiles = Array.from(fileMap.values()).sort(
+    (a, b) => b.position - a.position
+  );
+  for (const [_, item] of sortedFiles.entries()) {
+    for (const space of freeSpaces) {
+      if (item.numbers.length <= space.length && item.position > space.start) {
+        list.fill('.', item.position, item.position + item.numbers.length);
+        list.fill(
+          item.numbers[0],
+          space.start,
+          space.start + item.numbers.length
+        );
+
+        space.start += item.numbers.length;
+        space.length -= item.numbers.length;
+        break;
+      }
+    }
+  }
+
+  for (const [i, item] of list.entries()) {
+    if (item !== '.') result += +item * i;
+  }
+
+  return result;
+};
+
 function processData(data: string): string[] {
   const list = [];
   let id = 0;
@@ -34,21 +91,3 @@ function processData(data: string): string[] {
   }
   return list;
 }
-export const day9ProblemTwo = (data: string, result: number): number => {
-  const list = processData(data);
-  const listMap = new Map<string, string[]>();
-  for (const item in list) {
-    console.log(item);
-
-    // if (item !== '.') {
-    //   if (listMap.get(item)) {
-    //     listMap.get(item)?.push(item);
-    //   } else {
-    //     listMap.set(item, [item]);
-    //   }
-    // } else {
-    // }
-  }
-
-  return result;
-};
